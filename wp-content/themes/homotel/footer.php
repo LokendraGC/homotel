cf5007eecfdfae2b29b000eefa61969ce4911fdf
Lokendra GC <gclokendra10@gmail.com>
2024-09-04 15:58:25 +0545

    footer page updated

--- wp-content/themes/homotel/footer.php
+++ wp-content/themes/homotel/footer.php
@@ -17,25 +17,45 @@
 		<div class="row g-4 align-items-center">
 			<div class="col-md-12">
 				<div class="d-lg-flex align-items-center justify-content-between text-center">
-					<div>
-						<h3 class="fs-20">Address</h3>
-						Kathmandu Nepal
-					</div>
-					<div>
-						<h3 class="fs-20">Connect With Us</h3>
-						<div class="social-icons mb-sm-30 mt-4">
-							<a href="https://www.facebook.com/ApartmentHotels/" target="_blank"><i class="fa-brands fa-facebook-f"></i></a>
-							<a href="#"><i class="fa-brands fa-instagram"></i></a>
-							<a href="#"><i class="fa-brands fa-twitter"></i></a>
-							<a href="#"><i class="fa-brands fa-youtube"></i></a>
+					<?php if( $address = get_field('wtn_address','options') ): ?>
+						<div>
+							<h3 class="fs-20">Address</h3>
+							<?php echo $address; ?>
 						</div>
+					<?php endif; ?>
 
-					</div>
-					<div>
-						<h3 class="fs-20">Contact Us</h3>
-						Phone. <a href="">+9779704141584</a> <br>
-						Email. <a href="">info@homotel.com.np</a>
-					</div>
+					<?php if( have_rows('wtn_social_media','options') ): ?>
+						<div>
+							<h3 class="fs-20">Connect With Us</h3>
+							<div class="social-icons mb-sm-30 mt-4">
+								<?php while( have_rows('wtn_social_media','options') ):the_row();
+
+									$media_icon = get_sub_field('wtn_media','options');
+									$media_link = get_sub_field('wtn_link','options');
+									if( $media_link ){
+										$url = $media_link;
+										$target = '_blank';
+									}else{
+										$url = '#';
+										$target = '_self';
+									}
+									?>
+									<a href="<?php echo $url; ?>" target="<?php echo $target; ?>"><i class="fa-brands <?php echo $media_icon; ?>"></i></a>
+								<?php endwhile; ?>
+
+							</div>
+						</div>
+					<?php endif; ?>
+					<?php
+					$phone = get_field('wtn_phone_number','options');
+					$email = get_field('wtn_email_address','options');
+					if( $phone && $email ): ?>
+						<div>
+							<h3 class="fs-20">Contact Us</h3>
+							Phone. <a href="tel:<?php echo $phone; ?>"><?php echo $phone; ?></a> <br>
+							Email. <a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+						</div>
+					<?php endif; ?>
 				</div>
 			</div>                    
 		</div>
@@ -44,7 +64,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12 text-center">
-					Copyright 2024 Homotel. All Rights Reserved 
+					Copyright <?php echo Date('Y'); ?> Homotel. All Rights Reserved 
 				</div>
 			</div>
 		</div>
